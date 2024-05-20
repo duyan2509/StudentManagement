6254341ade4c71502014ad8b70ef44ba48015e29
UIT-21522640 <21522640@gm.uit.edu.vn>
2024-05-20 15:32:41 +0700

    lecture_detail_submission

--- app/src/main/java/com/example/studentmanagement/LectureDetailSubmissionFragment.java
+++ app/src/main/java/com/example/studentmanagement/LectureDetailSubmissionFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DetailClassDocumentFragment#newInstance} factory method to
+ * Use the {@link LectureDetailSubmissionFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DetailClassDocumentFragment extends Fragment {
+public class LectureDetailSubmissionFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class DetailClassDocumentFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DetailClassDocumentFragment() {
+    public LectureDetailSubmissionFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class DetailClassDocumentFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailClassDocumentFragment.
+     * @return A new instance of fragment LectureDetailSubmissionFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DetailClassDocumentFragment newInstance(String param1, String param2) {
-        DetailClassDocumentFragment fragment = new DetailClassDocumentFragment();
+    public static LectureDetailSubmissionFragment newInstance(String param1, String param2) {
+        LectureDetailSubmissionFragment fragment = new LectureDetailSubmissionFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class DetailClassDocumentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_class_document, container, false);
+        return inflater.inflate(R.layout.fragment_lecture_detail_submission, container, false);
     }
 }
